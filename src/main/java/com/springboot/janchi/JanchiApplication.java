f1a34b8886f00f695cdfc591c3b1f4b8bcac004a
chajihyun <gh8526@hanmail.net>
2025-08-15 00:07:00 +0900

    swagger test commit
    first commit

---
+++ src/main/java/com/springboot/janchi/JanchiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.janchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JanchiApplication.class, args);
+    }
+
+}
