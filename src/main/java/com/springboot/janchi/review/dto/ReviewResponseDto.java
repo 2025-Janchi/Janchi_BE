a1c18238328e20f8773b6670040e39a7db574e50
김민재 <redvelvet0625@gmail.com>
2025-08-19 01:31:07 +0900

    🚑 fix createdate type
    - createDate YYYY-MM-DD 형식으로 변경

--- src/main/java/com/springboot/janchi/review/dto/ReviewResponseDto.java
+++ src/main/java/com/springboot/janchi/review/dto/ReviewResponseDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -14,6 +15,16 @@ public class ReviewResponseDto {
     private String userId;
     private Integer star;
     private String content;
-    private Date createDate;
+    private String createDate;
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    public ReviewResponseDto(Long id, String userId, int star, String content, Date createDate) {
+        this.id = id;
+        this.userId = userId;
+        this.star = star;
+        this.content = content;
+        this.createDate = sdf.format(createDate);
+    }
 
 }
