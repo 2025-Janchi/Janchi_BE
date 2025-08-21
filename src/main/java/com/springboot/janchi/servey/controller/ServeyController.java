5a8592e50c0cbe3fcca0b3e6e7a532aeba7d0b5c
chajihyun <gh8526@hanmail.net>
2025-08-22 00:52:40 +0900

    코스 추천
    코스 추천

--- src/main/java/com/springboot/janchi/servey/controller/ServeyController.java
+++ src/main/java/com/springboot/janchi/servey/controller/ServeyController.java
@@ -1,24 +1,31 @@
 package com.springboot.janchi.servey.controller;
 
+import com.springboot.janchi.servey.dto.CourseResponseDto;
 import com.springboot.janchi.servey.dto.RecommendationResponseDto;
 import com.springboot.janchi.servey.dto.SurveyRequestDto;
+import com.springboot.janchi.servey.service.CourseService;
 import com.springboot.janchi.servey.service.JanchiRecommendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/recommendations")
+@RequestMapping("/api/v1/recommend")
 @RequiredArgsConstructor
 public class ServeyController {
     private final JanchiRecommendService service;
+    private final CourseService courseService;
 
-    @PostMapping
+    @PostMapping("/janchis")
     public ResponseEntity<RecommendationResponseDto> recommend(@RequestBody SurveyRequestDto req) {
         return ResponseEntity.ok(service.recommend(req));
+    }
+
+    @GetMapping("/{id}/course")
+    public ResponseEntity<CourseResponseDto> getCourse(@PathVariable Long id) throws IOException {
+        return ResponseEntity.ok(courseService.getCourse(id));
     }
 
 }
